2f364652606dab9a3b66653bfe03e01762855841
rahul1715 <87025601+rahul1715@users.noreply.github.com>
2021-09-29 11:02:48 +0530

    new

---
+++ assets/js/tag.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function(){
+alert($('#input').tagsinput('items'));
+});
+jQuery(function($) {
+
+$('#tags input').on('focusout', function() {
+ var txt = this.value.replace(/[^a-zA-Z0-9\+\-\.\#]/g, ''); // allowed characters list
+ if (txt) $(this).before('<span class="tag">' + txt + '</span>');
+ this.value = "";
+ this.focus();
+}).on('keyup', function(e) {
+ // comma|enter (add more keyCodes delimited with | pipe)
+ if (/(188|13)/.test(e.which)) $(this).trigger('focusout');
+});
+
+$('#tags').on('click', '.tag', function() {
+ if (confirm("Really delete this tag?")) $(this).remove();
+});
+
+});
