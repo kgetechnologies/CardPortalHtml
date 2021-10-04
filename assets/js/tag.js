63036ca0bd5d74ed3ad229c85582fcd93c8b42b5
rahul1715 <87025601+rahul1715@users.noreply.github.com>
2021-10-04 10:52:31 +0530

    new

--- assets/js/tag.js
+++ assets/js/tag.js
@@ -20,3 +20,24 @@ $('#tags').on('click', '.tag', function() {
 });
 
 });
+/**/
+$(document).ready(function(){
+alert($('#input').tagsinput('items'));
+});
+jQuery(function($) {
+
+$('#tags_1 input').on('focusout', function() {
+ var txt = this.value.replace(/[^a-zA-Z0-9\+\-\.\#]/g, ''); // allowed characters list
+ if (txt) $(this).before('<span class="tag">' + txt + '</span>');
+ this.value = "";
+ this.focus();
+}).on('keyup', function(e) {
+ // comma|enter (add more keyCodes delimited with | pipe)
+ if (/(188|13)/.test(e.which)) $(this).trigger('focusout');
+});
+
+$('#tags_1').on('click', '.tag', function() {
+ if (confirm("Really delete this tag?")) $(this).remove();
+});
+
+});
