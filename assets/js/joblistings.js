d7d791f558215d21647aa83fab1d25a13074e466
Aditya-kore <66473307+Aditya-kore@users.noreply.github.com>
2021-11-19 11:21:19 +0530

    Update joblistings.js

--- assets/js/joblistings.js
+++ assets/js/joblistings.js
@@ -1,6 +1,7 @@
-var json = []
-fetch("http://localhost:8000/assets/json/JobList.json").then(response => json = response.json())
-console.log(json);
+const json = [];
+parseJson();
+showJobs();
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "500px";
 }
@@ -9,10 +10,27 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-function showJobs() {
-
+function addToArray(arr) {
+    json.push(arr);
 }
 
 function parseJson() {
-    
+    fetch("http://localhost:8000/assets/json/JobList.json")
+    .then(response => {
+       return response.json();
+    })
+    .then(data => data['Jobs'].forEach(addToArray));
+    console.log(json);
+}
+
+function showJobs() {
+    var listNode = document.getElementById("jobListLeft");
+    var listItem = document.createElement("LIST")
+    var text = document.createTextNode("Hello");
+
+    listNode.innerHTML = '';
+    listItem.setAttribute("class","list-group-item");
+
+    listItem.appendChild(text);
+    listNode.appendChild(listItem);
 }
